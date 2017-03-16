569ecb5c7859fc649d82dff6e9a6ccff03469667
jonathan-morgangit config --global --add user.name jonathan-morgangit config --global --add user.name jonathan-morgan <morganusn@gmail.com>
2017-03-16 12:45:10 +0000

    Added Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316114110) do
+ActiveRecord::Schema.define(version: 20170316124214) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
