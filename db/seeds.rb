b2136e60873b593134654909257a60004801ccf0
jonathan-morgangit config --global --add user.name jonathan-morgangit config --global --add user.name jonathan-morgan <morganusn@gmail.com>
2017-03-16 18:09:38 +0000

    Custom theme

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,55 +1,40 @@
-#---
-# Excerpted from "Agile Web Development with Rails 5",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/rails5 for more book information.
-#---
-# encoding: utf-8
+
 Product.delete_all
-Product.create(title: 'Rails, Angular, Postgres, and Bootstrap',
+Product.create(title: 'Tomb Raider',
   description:
     %{<p>
-      <em>Powerful, Effective, and Efficient Full-Stack Web Development</em>
-      As a Rails developer, you care about user experience and performance,
-      but you also want simple and maintainable code. Achieve all that by
-      embracing the full stack of web development, from styling with
-      Bootstrap, building an interactive user interface with AngularJS, to
-      storing data quickly and reliably in PostgreSQL. Take a holistic view of
-      full-stack development to create usable, high-performing applications,
-      and learn to use these technologies effectively in a Ruby on Rails
-      environment.
+      <em>On Playstation</em>
+     Armed with only the raw instincts and physical ability, Tomb Raider delivers an intense and gritty story into the origins of Lara Croft and her ascent from a frightened young woman to a hardened survivor.
       </p>},
-  image_url: 'dcbang.jpg',    
-  price: 45.00)
+  image_url: 'tombraider.jpg',    
+  price: 60.00)
 # . . .
-Product.create(title: 'Seven Mobile Apps in Seven Weeks',
+Product.create(title: 'Bio Shock',
   description:
     %{<p>
-      <em>Native Apps, Multiple Platforms</em>
-      Answer the question “Can we build this for ALL the devices?” with a
-      resounding YES. This book will help you get there with a real-world
-      introduction to seven platforms, whether you’re new to mobile or an
-      experienced developer needing to expand your options. Plus, you’ll find
-      out which cross-platform solution makes the most sense for your needs.
+      <em>On XBox</em>
+      BioShock is set in 1960. The player guides the protagonist, Jack, after his airplane crashes in the ocean near the bathysphere terminus that leads to the underwater city of Rapture.
       </p>},
-  image_url: '7apps.jpg',
-  price: 26.00)
+  image_url: 'bioshock.jpg',
+  price: 50.00)
 # . . .
 
-Product.create(title: 'Ruby Performance Optimization',
+Product.create(title: 'Mass Effect',
   description:
     %{<p>
-      <em>Why Ruby Is Slow, and How to Fix It</em> 
-      You don’t have to accept slow Ruby or Rails performance. In this
-      comprehensive guide to Ruby optimization, you’ll learn how to write
-      faster Ruby code—but that’s just the beginning. See exactly what makes
-      Ruby and Rails code slow, and how to fix it. Alex Dymo will guide you
-      through perils of memory and CPU optimization, profiling, measuring,
-      performance testing, garbage collection, and tuning. You’ll find that
-      all those “hard” things aren’t so difficult after all, and your code
-      will run orders of magnitude faster.
+      <em>On PC/em> 
+  Mass Effect begins when Shepard is sent to the human colony of Eden Prime to recover an unearthed Prothean beacon. After landing on the planet, Shepard and Kaidan meet Ashley, who reveals that the colony is under attack by a geth army.
       </p>},
-  image_url: 'adrpo.jpg',
-  price: 46.00)
+  image_url: 'masseffect.jpg',
+  price: 45.00)
+
+# . . .
+
+Product.create(title: 'Rise of the Gardians',
+  description:
+    %{<p>
+      <em>On Xbox</em> 
+  Jack Frost awakens from a frozen pond with amnesia. Upon realizing no one can see or hear him, he disappears. Three hundred years later Jack, as the spirit of Winter, enjoys delivering snow days to school kids, but resents that they don't believe in him. At the North Pole, the Man in the Moon warns Nicholas St. North that Pitch Black is threatening the children of the world with his nightmares. He calls E. Aster Bunnymund, Sandy, and Tooth, to arms.
+      </p>},
+  image_url: 'riseofthegardians.jpg',
+  price: 32.00)
